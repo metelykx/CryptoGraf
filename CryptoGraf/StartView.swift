fb917a0df0eb836e58c52c4ad9ddac08dc40e168
metelykx <ivaschenkoodenys@gmail.com>
2025-08-04 11:16:41 +0300

    struct StartView

---
+++ CryptoGraf/StartView.swift
@@ -0,0 +1,19 @@
+//
+//  StartView.swift
+//  CryptoGraf
+//
+//  Created by Denis Ivaschenko on 04.08.2025.
+//
+
+import SwiftUI
+
+struct StartView: View {
+    var body: some View {
+        Text("ll")
+    }
+}
+
+
+#Preview {
+    StartView()
+}
